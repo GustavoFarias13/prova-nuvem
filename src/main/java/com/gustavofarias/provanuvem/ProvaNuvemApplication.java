bd0b6ebe1c53821bb57c14bf2fefe6025866dadc
Gustavo Farias <gustavojosefariasdesouza@gmail.com>
2025-09-29 13:54:40 -0300

    initial commit

---
+++ src/main/java/com/gustavofarias/provanuvem/ProvaNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.gustavofarias.provanuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaNuvemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaNuvemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavofarias/provanuvem/ProvaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavofarias.provanuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaNuvemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
